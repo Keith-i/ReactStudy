97b87484c9218e03711a0e7c0eebbba45a58f811
Keith <754781293@qq.com>
2020-04-29 11:20:57 +0800

    提交

--- src/keyDemo.jsx
+++ src/keyDemo.jsx
@@ -1,9 +1,58 @@
 import React, { Component } from 'react'
 
 class keyDemo extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      userInfo: [
+        {
+          name: 'Keith',
+          age: 22,
+          sex: '男',
+          hobby: ['爬山', '游泳', '看书']
+        },
+        {
+          name: 'Jenny',
+          age: 22,
+          sex: '女',
+          hobby: ['美食', '旅游', '购物']
+        },
+        {
+          name: 'Tom',
+          age: 22,
+          sex: '男',
+          hobby: ['美女', '二次元', '游戏']
+        }
+      ]
+    }
+  }
+
   render() {
     return (
-      <div>keyDemo</div>
+      <div>
+        <p>列表渲染</p>
+        <ul>
+          {
+            this.state.userInfo.map((item, index) => {
+              return (
+                <li key={index}>
+                  <span>{item.name}</span>&nbsp;&nbsp;
+                  <span>{item.age}</span>&nbsp;&nbsp;
+                  <span>{item.sex}</span>
+                  <div>
+                    {
+                      item.hobby.map((childItem, childIndex) => {
+                        return <i key={childIndex}>{childItem + ' '}</i>
+                      })
+                    }
+                  </div>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
     )
   }
 }
